52b7c4dd7bbc34048561b7b5a0805a5fb141cfc8
Martin Price <github@systemhorizons.co.uk>
2023-10-13 19:40:12 +0100

    Minor PHPCS fixes (#15)

--- simplify.api.php
+++ simplify.api.php
@@ -7,16 +7,16 @@
 /**
  * Alter the list of fields that can be hidden.
  *
- * @param $fields
+ * @param array $fields
  *   An associative array of fields that can be hidden, where the key is the
  *   machine name of the field and the value is the human-readable name of the
  *   field.
- * @param $type
+ * @param string $type
  *   The type of fields passed to the $fields parameter. Can be one of:
  *   nodes, users, comments, taxonomy, blocks.
  *   See simplify_get_fields() for examples.
  */
-function hook_simplify_get_fields_alter(&$fields, $type) {
+function hook_simplify_get_fields_alter(array &$fields, $type) {
   // Allow our module's custom 'foo' node field to be hidden.
   if ($type == 'nodes') {
     $fields['foo'] = t('Foo field');
@@ -27,14 +27,14 @@ function hook_simplify_get_fields_alter(&$fields, $type) {
  * Alter the way fields are hidden, or hide fields defined in
  * hook_simplify_get_fields_alter().
  *
- * @param $form
+ * @param array $form
  *   The form array in which the field to be hidden resides. Hiding a field is
  *   generally done by setting its '#access' form value to FALSE.
- * @param $field
+ * @param string $field
  *   The machine name of the field to be hidden as defined in
  *   simplify_get_fields() or hook_simplify_get_fields_alter().
  */
-function hook_simplify_hide_field_alter(&$form, $field) {
+function hook_simplify_hide_field_alter(array &$form, $field) {
   // Hide our module's custom 'foo' node field.
   if ($field == 'foo') {
     $form['foo']['#access'] = FALSE;
